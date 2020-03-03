c919a62e6750575cee29c1462085a66cf78362d4
MathijsDaemen <61730652+MathijsDaemen@users.noreply.github.com>
2020-03-03 10:15:57 +0100

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        int number, temp;
+
+        Scanner sc = new Scanner(System.in);
+        Socket s = new Socket("127.0.0.1", 5000);
+        Scanner sc1 = new Scanner(s.getInputStream());
+
+        System.out.println("Enter a number: ");
+        number = sc.nextInt();
+
+        PrintStream p = new PrintStream(s.getOutputStream());
+        p.println(number);
+
+        temp = sc1.nextInt();
+        System.out.println(temp);
+    }
+}
