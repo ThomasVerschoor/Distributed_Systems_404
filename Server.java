990aceb7d14292bb9931ffe9f40aae61c8734c3c
Hertoghsl <61731569+Hertoghsl@users.noreply.github.com>
2020-03-24 13:06:34 +0100

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,25 +1,25 @@
-
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server {
-
     public static void main(String[] args) throws IOException {
-        int number, temp;
-
-        ServerSocket s1 = new ServerSocket(30027);
-        Socket ss = s1.accept();
-        Scanner sc = new Scanner(ss.getInputStream());
-        number = sc.nextInt();
-
-        temp = number*2;
-
-        PrintStream p = new PrintStream(ss.getOutputStream());
-        p.println(temp);
-
+        ServerSocket serverSocket = new ServerSocket(30028); //Poort van Server
+        while(true){
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept(); //ALS er een neiuwe connecie naar ServerSocket komt steken we in socket de socket van de Host die connecteerd
+                System.out.println("A new client is connected: " + socket);
+                DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+                DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
+                System.out.println("Assigning new thread for this client");
+                Thread thread = new ClientHandler(socket,dataIn,dataOut);   //We maken een nieuwe thread aan voor elke client die joinen
+                thread.start();                                             // met daarin een runnende Client handler die onze request zal regelen.
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
